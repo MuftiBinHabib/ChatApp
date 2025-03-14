468564de2ad9c03594528be0b74010428fa1e7ce
Mufti Bin Habib <muftibinhabib500@gmail.com>
2025-03-14 08:33:17 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Serve the built frontend
+app.use(express.static(path.join(__dirname, 'public')));  // If static files are in public/
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 io.on('connection', (socket) => {
   console.log('A user connected');
-  
-  // Emit message to all connected clients
+
   socket.on('message', (msg) => {
     io.emit('message', { text: msg, id: socket.id });
   });
@@ -19,6 +26,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(8080, '0.0.0.0', () => {
-  console.log('Server running on http://0.0.0.0:8080');
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
